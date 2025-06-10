0c9fe588dc76fd78137c6fd51576307b819bc1dc
Qadirdad-Kazi <154679341+Qadirdad-Kazi@users.noreply.github.com>
2025-06-10 21:08:56 +0500

    Revert "fix: enforce https for all cloudinary urls"
    This reverts commit 82f309fcef8b24a5938a958b5decdfe5aba1b6d0.

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -12,7 +12,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true, // Force HTTPS for all URLs
 });
 
 // Configure Multer to use Cloudinary for storage
